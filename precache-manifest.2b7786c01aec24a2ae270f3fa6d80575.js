57b732e9da517aef438d70669f28b6adb3938bbe
al <a.k.sugiharto@gmail.com>
2020-08-16 18:24:39 +0200

    Updates

--- precache-manifest.2b7786c01aec24a2ae270f3fa6d80575.js
+++ precache-manifest.2b7786c01aec24a2ae270f3fa6d80575.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "e0a6e099a7bca0ddc8ff7ef96a6eb68f",
+    "revision": "8649c09d59bd095ce20f720d8da3aad2",
     "url": "/pro-sultanbromptonstore/index.html"
   },
   {
-    "revision": "c035962e1074a64e863c",
+    "revision": "85be1865abc2fc1013a3",
     "url": "/pro-sultanbromptonstore/static/css/main.5ecd60fb.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/pro-sultanbromptonstore/static/js/2.26220f9e.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "c035962e1074a64e863c",
-    "url": "/pro-sultanbromptonstore/static/js/main.2205974b.chunk.js"
+    "revision": "85be1865abc2fc1013a3",
+    "url": "/pro-sultanbromptonstore/static/js/main.144b7f49.chunk.js"
   },
   {
     "revision": "d1b6a31f02c1f5ce199f",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/pro-sultanbromptonstore/precache-manifest.36de643d3637025bc91c269eb920ea49.js"
+  "/pro-sultanbromptonstore/precache-manifest.2b7786c01aec24a2ae270f3fa6d80575.js"
 );
 
 self.addEventListener('message', (event) => {
